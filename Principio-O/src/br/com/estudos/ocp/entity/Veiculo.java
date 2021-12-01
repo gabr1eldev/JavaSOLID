69a6f06c6cc36e34095f8a0fb4a16bcebfe46b53
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 16:54:46 -0300

    Criando os métodos/construtor/get/set

--- Principio-O/src/br/com/estudos/ocp/entity/Veiculo.java
+++ Principio-O/src/br/com/estudos/ocp/entity/Veiculo.java
@@ -2,6 +2,35 @@ package br.com.estudos.ocp.entity;
 
 abstract public class Veiculo {
 	
+	protected String cor;
+	protected String ano;
+	protected double motor;
+	
+	public Veiculo(String cor, String ano, double motor) {
+		this.cor = cor;
+		this.ano = ano;
+		this.motor = motor;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getAno() {
+		return ano;
+	}
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+	public double getMotor() {
+		return motor;
+	}
+	public void setMotor(double motor) {
+		this.motor = motor;
+	}
+	
 	
 
 }
