3f7152285d57d8699bf56affca58688b9b18f986
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 16:35:34 -0300

    Simulanndo um CRUD

---
+++ Principio-S/src/br/com/estudos/srp/DAO/CRUD.java
@@ -0,0 +1,32 @@
+package br.com.estudos.srp.DAO;
+
+public class CRUD {
+	
+	public void addCliente() {
+		
+	}
+	public void removerCliente() {
+		
+	}
+
+	public void  updateCliente() {
+		
+	}
+
+	public void listarCliente() {
+		
+	}
+
+	/*
+	 * esta classe simula um CRUD,então de acordo com o principio
+	 * da responsabilidade única,ela deve servir apenas como CRUD
+	 */
+	
+	/*aqui quebraria o principio da responsabilidade única
+	neste caro criaremos uma nova class para notificar o cliente
+	*/
+	public void notificarCliente() {
+		
+	}
+
+}
