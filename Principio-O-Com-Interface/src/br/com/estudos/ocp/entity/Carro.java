65a0f00fe29b35288b52746844198fd2158f81cc
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 18:24:08 -0300

    Criando os atributos e o toString e passando para dentro do metodo montarCarro onde ele irá para o construtor

--- Principio-O-Com-Interface/src/br/com/estudos/ocp/entity/Carro.java
+++ Principio-O-Com-Interface/src/br/com/estudos/ocp/entity/Carro.java
@@ -3,11 +3,64 @@ package br.com.estudos.ocp.entity;
 import br.com.estudos.ocp.utility.InterCarro;
 
 public class Carro implements InterCarro{
+	
+	private String cor;
+	private String ano;
+	private double motor;
+	private Integer assentos;
+
+	public Carro(String cor, String ano, double motor, Integer assentos) {
+		montarCarro(cor, ano, motor, assentos);
+	}
 
 	@Override
 	public void montarCarro(String cor, String ano, double motor, Integer assentos) {
+		this.cor = cor;
+		this.ano = ano;
+		this.motor = motor;
+		this.assentos = assentos;
 		
+		toString();
+		ligarMotor();
 		
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Carro [cor=" + cor + ", ano=" + ano + ", motor=" + motor + ", assentos=" + assentos + "]";
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public double getMotor() {
+		return motor;
+	}
+
+	public void setMotor(double motor) {
+		this.motor = motor;
+	}
+
+	public Integer getAssentos() {
+		return assentos;
+	}
+
+	public void setAssentos(Integer assentos) {
+		this.assentos = assentos;
 	}
 
 	@Override
