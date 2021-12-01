50258c74627a6bcb15ef17c4cfebbf4d1de7a47a
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 17:21:21 -0300

    Pegando o Enum para saber qual será o tipo de veículo

--- Principio-O/src/br/com/estudos/ocp/main/Main.java
+++ Principio-O/src/br/com/estudos/ocp/main/Main.java
@@ -1,9 +1,11 @@
 package br.com.estudos.ocp.main;
 
-public class Main {
+import br.com.estudos.ocp.utility.TipoDeVeiculo;
 
+public class Main {
+	private static TipoDeVeiculo tpVeiculo;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
