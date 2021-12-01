bc5737c4761a33d4a1f8247d0e8814cf4991b632
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 18:09:13 -0300

    Criando interface do carro

---
+++ Principio-O-Com-Interface/src/br/com/estudos/ocp/utility/InterCarro.java
@@ -0,0 +1,8 @@
+package br.com.estudos.ocp.utility;
+
+public interface InterCarro {
+	
+	public void montarCarro(String cor, String ano, double motor,Integer assentos);
+	public void ligarMotor();
+
+}
