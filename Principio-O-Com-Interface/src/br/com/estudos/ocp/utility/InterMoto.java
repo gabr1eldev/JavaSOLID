e22b0c37386a9699cbcfd0ec40c8799b1cc5b581
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 18:11:08 -0300

    Criando a interface da moto

---
+++ Principio-O-Com-Interface/src/br/com/estudos/ocp/utility/InterMoto.java
@@ -0,0 +1,8 @@
+package br.com.estudos.ocp.utility;
+
+public interface InterMoto {
+	
+	public void montarMoto(String cor, String ano, double motor,String tipoDeFreio);
+	public void ligarMotor();
+
+}
