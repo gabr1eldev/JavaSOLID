108731604b0a3005c41c874b1778b03e2891e968
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 17:24:19 -0300

    Criando atributos com JoptioPane onde exibirá um dialogo para ficar dinamico

--- Principio-O/src/br/com/estudos/ocp/main/Main.java
+++ Principio-O/src/br/com/estudos/ocp/main/Main.java
@@ -1,10 +1,21 @@
 package br.com.estudos.ocp.main;
 
+import javax.swing.JOptionPane;
+
 import br.com.estudos.ocp.utility.TipoDeVeiculo;
 
 public class Main {
 	private static TipoDeVeiculo tpVeiculo;
 	public static void main(String[] args) {
+		String veiculo = JOptionPane.showInputDialog("Escolha entre Moto e Carro");
+		String ano = JOptionPane.showInputDialog("qual o Ano");				
+		String cor = JOptionPane.showInputDialog("Escolha a Cor");
+		String motorCarro = JOptionPane.showInputDialog("Quantos Cavalos");
+		String portas = JOptionPane.showInputDialog("Quantas Portas");
+		String freio = JOptionPane.showInputDialog("Qual o tipo de Freio");
+		String motorMoto = JOptionPane.showInputDialog("Quantas cilindradas");
+		
+		tpVeiculo = TipoDeVeiculo.valueOf(veiculo);
 		
 
 	}
