1c3c984b98b79a01ce42d99aaa869bf704184a14
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 17:08:46 -0300

    Criando a class Moto herdando da class Veiculo

---
+++ Principio-O/src/br/com/estudos/ocp/entity/Moto.java
@@ -0,0 +1,21 @@
+package br.com.estudos.ocp.entity;
+
+public class Moto extends Veiculo{
+	private String tipoDeFreio;
+
+	public Moto(String cor, String ano, double motor,String tipoDeFreio) {
+		super(cor, ano, motor);
+		this.tipoDeFreio = tipoDeFreio;
+	}
+
+	public String getTipoDeFreio() {
+		return tipoDeFreio;
+	}
+
+	public void setTipoDeFreio(String tipoDeFreio) {
+		this.tipoDeFreio = tipoDeFreio;
+	}
+	
+	
+
+}
