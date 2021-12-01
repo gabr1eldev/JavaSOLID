ba1ca9d641f40cdd3041ae2cadbcc1faf2cd3f84
greg <gabriel.gregorio1987@gmail.com>
2021-12-01 16:42:10 -0300

    Criando uma class de notificação

---
+++ Principio-S/src/br/com/estudos/srp/utility/notificacao.java
@@ -0,0 +1,12 @@
+package br.com.estudos.srp.utility;
+
+public class notificacao {
+	/*
+	 * aqui seria a maneira correta de notificar o cliente,retirando o método do CRUD e criando uma nova class
+	 */
+
+	public void notificarCliente() {
+
+	}
+
+}
