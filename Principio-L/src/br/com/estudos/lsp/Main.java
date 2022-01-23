6adb0af091d8f6e3a72df8438c22371e26d33836
greg <gabriel.gregorio1987@gmail.com>
2022-01-23 15:59:25 -0300

    testando o NubankRewards, ok!

--- Principio-L/src/br/com/estudos/lsp/Main.java
+++ Principio-L/src/br/com/estudos/lsp/Main.java
@@ -2,12 +2,14 @@ package br.com.estudos.lsp;
 
 import br.com.estudos.lsp.entity.CreditCard;
 import br.com.estudos.lsp.entity.DebitCard;
+import br.com.estudos.lsp.entity.NubankRewards;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		//CreditCard card = new CreditCard();
-		DebitCard card = new DebitCard();
+		//DebitCard card = new DebitCard();
+		NubankRewards card = new NubankRewards();
 		
 		card.validarCartao();
 		card.coletarPagamento();
