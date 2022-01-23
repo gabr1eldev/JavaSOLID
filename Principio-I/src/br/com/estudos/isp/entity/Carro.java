273c76034a292ac956947bfc21fcb4f0ccbbf9e2
greg <gabriel.gregorio1987@gmail.com>
2022-01-23 20:16:41 -0300

    configurando uma moto

--- Principio-I/src/br/com/estudos/isp/entity/Carro.java
+++ Principio-I/src/br/com/estudos/isp/entity/Carro.java
@@ -1,5 +1,41 @@
 package br.com.estudos.isp.entity;
 
-public class Carro {
+import br.com.estudos.isp.utility.Iveiculo;
+
+
+public class Carro implements Iveiculo {
+	
+	private String color;
+	private String year;
+	private int seats;
+	private double engine;
+	
+	
+
+	public Carro(String color, String year, int seats, double engine) {
+		configureCar(color, seats, year, engine);
+	}
+
+	@Override
+	public void configureCar(String color, int seats, String year, double engine) {
+		this.color = color;
+		this.seats = seats;
+		this.year = year;
+		this.engine = engine;
+		System.out.println("Criando um carro" + engine + " " + color + " " + year + " " + seats);
+		
+	}
+
+	@Override
+	public void configureMoto(String color, String year, double engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarVeiculo() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Principio-I/src/br/com/estudos/isp/entity/Moto.java
+++ Principio-I/src/br/com/estudos/isp/entity/Moto.java
@@ -1,5 +1,38 @@
 package br.com.estudos.isp.entity;
 
-public class Moto {
+import br.com.estudos.isp.utility.Iveiculo;
+
+public class Moto implements Iveiculo{
+	
+	private String color;
+	private String year;
+	private double engine;
+	
+	
+
+	public Moto(String color, String year, double engine) {
+		configureMoto(color, year, engine);
+	}
+
+	@Override
+	public void configureMoto(String color, String year, double engine) {
+		this.color = color;
+		this.year = year;
+		this.engine = engine;
+		System.out.println("Criando uma Moto" + engine + " " + color + " " + year);
+		iniciarVeiculo();
+		
+	}
+
+	public void configureCar(String color, int seats, String year, double engine) {
+		
+		
+	}
+
+	@Override
+	public void iniciarVeiculo() {
+		System.out.println("Ligando os Motores");
+		
+	}
 
 }
