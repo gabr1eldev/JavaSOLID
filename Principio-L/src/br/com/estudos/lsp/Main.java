5dece5ca0484b7940802bd889d7c7e28f496218d
greg <gabriel.gregorio1987@gmail.com>
2022-01-23 15:18:09 -0300

    Testando o DebitCard

--- Principio-L/src/br/com/estudos/lsp/Main.java
+++ Principio-L/src/br/com/estudos/lsp/Main.java
@@ -1,11 +1,13 @@
 package br.com.estudos.lsp;
 
 import br.com.estudos.lsp.entity.CreditCard;
+import br.com.estudos.lsp.entity.DebitCard;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		CreditCard card = new CreditCard();
+		//CreditCard card = new CreditCard();
+		DebitCard card = new DebitCard();
 		
 		card.validarCartao();
 		card.coletarPagamento();
