16e850d87c99af229481f5aefd83a58d432f977e
greg <gabriel.gregorio1987@gmail.com>
2022-01-23 22:16:15 -0300

    Criando a class de alto nível

--- Principio-D/src/br/com/estudos/dip/entity/MySQLProduct.java
+++ Principio-D/src/br/com/estudos/dip/entity/MySQLProduct.java
@@ -1,5 +1,11 @@
 package br.com.estudos.dip.entity;
 
 public class MySQLProduct {
+	
+	//apenas como exemplo,essa seria uma class de baixo nível
+	
+	public String getProductById(String id) {
+		return "MSQL: Exibindo os dados do produto" + id;
+	}
 
 }
--- Principio-D/src/br/com/estudos/dip/payment/Payment.java
+++ Principio-D/src/br/com/estudos/dip/payment/Payment.java
@@ -1,5 +1,17 @@
 package br.com.estudos.dip.payment;
 
+import br.com.estudos.dip.entity.MySQLProduct;
+
 public class Payment {
+	
+	//aqui só como exemplo,é uma class de alto nível chamando uma class de baixo nível,para fazer a consulta
+	
+	
+	
+	public void pay(String productId) {
+		MySQLProduct dbProduct = new MySQLProduct();
+		String product = dbProduct.getProductById(productId);
+		System.out.println(product);
+	}
 
 }
