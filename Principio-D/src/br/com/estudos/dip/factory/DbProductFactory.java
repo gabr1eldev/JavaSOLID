36a0e77e132d4531f477af411991fb9073222781
greg <gabriel.gregorio1987@gmail.com>
2022-01-23 22:34:19 -0300

    Criando uma situação de escolha de banco

--- Principio-D/src/br/com/estudos/dip/factory/DbProductFactory.java
+++ Principio-D/src/br/com/estudos/dip/factory/DbProductFactory.java
@@ -1,5 +1,18 @@
 package br.com.estudos.dip.factory;
 
+import br.com.estudos.dip.entity.MongoDBProduct;
+import br.com.estudos.dip.entity.MySQLProduct;
+import br.com.estudos.dip.utility.Db;
+import br.com.estudos.dip.utility.IDbProduct;
+
 public class DbProductFactory {
+	
+	public static IDbProduct create(Db type) {
+		if (type == Db.MYSQL) {
+			return new MySQLProduct();
+		} else {
+			return new MongoDBProduct();
+		}
+	}
 
 }
